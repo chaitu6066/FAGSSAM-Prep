a2f7e2bb481916697e1f0cd2985095556211a7d7
fagssamready <90529292+fagssamready@users.noreply.github.com>
2021-09-24 05:57:40 +0530

    1. Get the starting node of a loop 2. Intersection of 2 linkedlists 3. Print linkedlist from end.

--- Data Structures/src/com/ds/LL/SingleLinkedList.java
+++ Data Structures/src/com/ds/LL/SingleLinkedList.java
@@ -75,13 +75,7 @@ public class SingleLinkedList {
         sll.appendAtTail(20);
         sll.appendAtTail(30);
         sll.appendAtTail(40);
-        sll.print();
-        if (sll.deleteNode(20)) {
-            System.out.println("20 deleted from list");
-        }
         sll.appendAtTail(10);
-
-        sll.print();
         sll.removeDuplicatesWithoutExtraSpace(sll).print();
         sll.appendAtTail(10);
         sll.appendAtTail(10);
@@ -91,11 +85,12 @@ public class SingleLinkedList {
         sll.appendAtTail(10);
         sll.appendAtTail(1000);
         sll.appendAtTail(10);
-        sll.print();
+
         sll.printKthlastNodeIntheList(sll, 2);
         sll.reverseASLL(sll).print();
-
         sll.addTwoLists();
+        sll.loopCheckHelper();
+        sll.getIntersectionNodeHelper();
     }
 
 /* ********************* Problems *********************/
@@ -187,6 +182,8 @@ public class SingleLinkedList {
  * single digit. Return the result in the form of new list.
  ************************/
 
+    //TODO: Implement the followup questions 1. If the length of the LL are not same?
+
     void addTwoLists() {
         SLLNode h1 = new SLLNode(4);
         h1.next = new SLLNode(2);
@@ -199,10 +196,12 @@ public class SingleLinkedList {
         h2.next.next.next = new SLLNode(5);
 
         SLLNode result = sumOfTwoLLRecursive(h1, h2);
+        System.out.print("Sum of 2 LLs is: \t");
         while (result != null) {
             System.out.print("\t" + result.data);
             result = result.next;
         }
+        System.out.println();
     }
 
     SLLNode sumOfTwoLLRecursive(SLLNode l1, SLLNode l2) {
@@ -231,16 +230,164 @@ public class SingleLinkedList {
 
 /* ********** Find out that the Linkedlist is null terminated or has a loop ************/
 
-    boolean hasLoop(SLLNode head) {
-        SLLNode slow = head, fast = head;
+    void loopCheckHelper() {
+        SLLNode s1 = new SLLNode(1);
+        SLLNode s2 = new SLLNode(2);
+        s1.next = s2;
+        SLLNode s3 = new SLLNode(3);
+        s2.next = s3;
+        SLLNode s4 = new SLLNode(4);
+        s3.next = s4;
+        SLLNode s5 = new SLLNode(5);
+        s4.next = s5;
+        SLLNode s6 = new SLLNode(6);
+        s5.next = s6;
+        SLLNode s7 = new SLLNode(7);
+        s6.next = s7;
+        SLLNode s8 = new SLLNode(8);
+        s7.next = s8;
+        SLLNode s9 = new SLLNode(9);
+        s8.next = s9;
+        s9.next = s4;// This is loop creation. It should be null. But we are pointing to s4
+        // again to create loop.
 
-        while ( slow != fast || fast == null || fast.next != null) {
+        if (hasLoop(s1)){
+            System.out.println("Loop starting Node: " + getLoopStartingNode(s1).data);
+        }
+
+    }
+
+    boolean hasLoop(SLLNode cHead) {
+        SLLNode slow = cHead, fast = cHead;
+
+        while ( fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
         }
 
         return slow == fast;
     }
 
+
+    SLLNode getLoopStartingNode(SLLNode cHead) {
+        SLLNode slow = cHead, fast = cHead;
+
+        while ( fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        slow = cHead;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+/* ********* How do you find the middle node of a linked list? Ans : Runner technique. Implement
+slow pointer and fast pointer answer
+ */
+    //TODO: Implement Fast and Slow Pointer: Ref: Above method hasLoop()
+
+
+
+    /* ****** Find out whether 2 linked list intersect or not. If yes, return the starting point
+    of the intersection.
+     */
+
+
+   void getIntersectionNodeHelper() {
+       SLLNode s1 = new SLLNode(1);
+       SLLNode s2 = new SLLNode(2);
+       s1.next = s2;
+       SLLNode s3 = new SLLNode(3);
+       s2.next = s3;
+       SLLNode s4 = new SLLNode(4);
+       s3.next = s4;
+       SLLNode s5 = new SLLNode(5);
+       s4.next = s5;
+       SLLNode s6 = new SLLNode(6);
+       s5.next = s6;
+       SLLNode s7 = new SLLNode(7);
+       s6.next = s7;
+       SLLNode s8 = new SLLNode(8);
+       s7.next = s8;
+       SLLNode s9 = new SLLNode(9);
+       s8.next = s9;
+
+       SLLNode l1 = new SLLNode(11);
+       SLLNode l2 = new SLLNode(12);
+       l1.next = l2;
+       SLLNode l3 = new SLLNode(13);
+       l2.next = l3;
+       l3.next = s7;
+
+       SLLNode result = getIntersectionNode(l1, s1);
+       if (result != null) {
+           System.out.println("Intersection Starting Node: " + result.data);
+       }
+   }
+
+    SLLNode getIntersectionNode(SLLNode l1, SLLNode l2) {
+        if (l2 == null || l1 == null)
+            return null;
+
+        int l1Len = getLLLength(l1);
+        int l2Len = getLLLength(l2);
+
+        SLLNode shortLL, longLL;
+
+        if (l1Len < l2Len) {
+            shortLL = l1;
+            longLL = l2;
+        } else {
+            shortLL = l2;
+            longLL = l1;
+        }
+
+        int diff = Math.abs(l1Len - l2Len);
+
+        while (diff > 0) {
+            longLL = longLL.next;
+            diff--;
+        }
+
+        while (shortLL != longLL && shortLL != null) {
+            shortLL = shortLL.next;
+            longLL = longLL.next;
+        }
+
+        if (shortLL == longLL) {
+            return shortLL;
+        }
+        return null;
+
+    }
+
+    int getLLLength(SLLNode l1) {
+        int len = 0;
+        while (l1 != null) {
+            len++;
+            l1 = l1.next;
+        }
+        return len;
+    }
+
+/* ************** Print LL from last **********/
+
+    void printLLFromTailRecur(SLLNode list) {
+       if (list == null) {
+           return;
+       }
+       printLLFromTailRecur(list.next);
+       System.out.println(list.data);
+    }
 
 }
